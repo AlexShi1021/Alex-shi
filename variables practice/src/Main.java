28ddebd1859722c64c3a32c7a0ffb688f2f1d8da
Alex Shi <1137834@learn.vsb.bc.ca>
2021-02-11 09:48:51 -0800

    variable and names practice assignment

---
+++ variables practice/src/Main.java
@@ -0,0 +1,17 @@
+class VariablesAndNames
+{
+    public static void main( String[] args )
+    {
+        int room;
+        double number;
+
+    room= 113;
+            number= 2.71828;
+
+
+
+        System.out.println(" This is room #" + room );
+            System.out.println("e is close to " + number);
+            System.out.println("i am learning a bit about computer_science");
+    }
+}
