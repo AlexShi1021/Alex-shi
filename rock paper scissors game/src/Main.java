f7d005a2053538d093b7df5549492c2b168980a0
Alex Shi <1137834@learn.vsb.bc.ca>
2021-02-22 08:49:43 -0800

    rock, paper scissors

---
+++ rock paper scissors game/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        //random numbers
+        int random, rock, paper, scissor;
+        random
+
+
+        System.out.println("welcome to rock paper scissors");
+        System.out.println((int) (Math.random()*3) + 1);
+
+    }
+}
