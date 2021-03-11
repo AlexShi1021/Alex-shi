651a7e5ac803632b218eba971a056f4a786bf4f6
Alex Shi <1137834@learn.vsb.bc.ca>
2021-03-11 09:09:13 -0800

    class practice
    classroom practice assignment

---
+++ Class Practice/src/Courses.java
@@ -0,0 +1,3 @@
+public class Courses {
+    public String courses;
+}
---
+++ Class Practice/src/Student.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class Student {
+
+    public String Firstname;
+    static int studentID = 1;
+    int idNum;
+
+    ArrayList<Classes> classlist = new ArrayList<>();
+
+
+    Student(){
+        Firstname = "John";
+        studentID = idNum;
+        idNum ++;
+    }
+
+    public void addclass()
+
+
+
+
+
+
+
+
+
+}
