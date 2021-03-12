78c8347f455ed3a0f251a571b5adf5a97923ccfd
AlexShi1021 <78816890+AlexShi1021@users.noreply.github.com>
2021-03-11 22:25:06 -0800

    Update Classes.java

--- Class Practice/src/Classes.java
+++ Class Practice/src/Classes.java
@@ -1,5 +1,14 @@
 
 public class Classes {
+  public String classSubject;
+    public int classgrade;
+
+
+public Classes(String courseName, int studentGrade) {
+    this.classSubject = classSubject;
+    this.classgrade = classgrade;
+
+}
 
 
 
