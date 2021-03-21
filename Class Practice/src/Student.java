eb35b48a3a288231abcbd2b305d45dac9598db5e
AlexShi1021 <78816890+AlexShi1021@users.noreply.github.com>
2021-03-20 20:00:32 -0700

    Update Student.java

--- Class Practice/src/Student.java
+++ Class Practice/src/Student.java
@@ -3,28 +3,32 @@ import java.util.ArrayList;
 public class Student {
 
     public String Firstname;
+    public String Lastname;
     static int studentID = 1;
     int idNum;
 
     ArrayList<Classes> classlist = new ArrayList<>();
 
 
-    Student(){
+   Student(){
         Firstname = "John";
-        studentID = idNum;
+        Lastname = "Doe";
         idNum ++;
+        studentID = idNum;
     }
 
-    public void addclass(String classSubject, int classgrade){
-
+    public String getfullname(){
+        return Firstname + " " + Lastname;
     }
 
+    public void addclass(String classSubject, int classgrade) {
+        if (classlist.size() > 8) {
+            System.out.println("Already more than 8 courses");
+        } else {
+            classlist.add(new Classes(classSubject, classgrade));
+
+        }
 
 
-
-
-
-
-
-
+    }
 }
