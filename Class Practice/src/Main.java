fad0ebd57cfeb52b118642ffaa50941d6c992294
AlexShi1021 <78816890+AlexShi1021@users.noreply.github.com>
2021-03-20 19:54:01 -0700

    Update Main.java

--- Class Practice/src/Main.java
+++ Class Practice/src/Main.java
@@ -1,33 +1,35 @@
-
 public class Main {
     public static void main(String[] args) {
         Student student = new Student();
 
-// prints out student information
+        // prints out student information
         System.out.println("student id # is " + student.idNum);
         System.out.println("Student name: " + student.getfullname());
 
 // prints out student grade for each class
+        
         System.out.println("-----Student grade on each class-----");
-        int[] grade = new int[8];
+        int[] grade = new int[4];
 
         for (int i = 0; i < grade.length; i++) {
 
             grade[i] = (int) (Math.random() * 100) + 1;
 
-            System.out.println(grade[i]);
-
         }
+        System.out.println("Science" + " " + grade[0]);
+        System.out.println("Math" + " " + grade[1]);
+        System.out.println("Socials" + " " + grade[2]);
+        System.out.println("computers" + " " + grade[3]);
 
         int average = 0;
 
         for (int i = 0; i < grade.length; i++) {
-            average = average + grade[i] / 8;
+            average = average + grade[i] / 4;
 
 
         }
         //prints out student average mark
-        System.out.println("the average grade is " + average);
+        System.out.println("the average grade is " + average + "%");
 
 
 
@@ -38,3 +40,6 @@ public class Main {
 
     }
 }
+
+
+
